3d98a1ce48383394a5ceef55d7b28bbcd8518064
Samarpan <samarpanchakravarty308@gmail.com>
2020-08-23 20:33:34 +0530

    Updates

---
+++ precache-manifest.a92e4a3af139cee86fe7b144bd945e41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f5b931ae2605c217658ffa87bf728e3",
+    "url": "/Movie-Dictionary/index.html"
+  },
+  {
+    "revision": "468a69f97cbf06902d41",
+    "url": "/Movie-Dictionary/static/css/main.89a4c321.chunk.css"
+  },
+  {
+    "revision": "c7472608c9989e1533a3",
+    "url": "/Movie-Dictionary/static/js/2.8ec2ec56.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movie-Dictionary/static/js/2.8ec2ec56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "468a69f97cbf06902d41",
+    "url": "/Movie-Dictionary/static/js/main.a4c3b4a5.chunk.js"
+  },
+  {
+    "revision": "3e0417971ed795b81998",
+    "url": "/Movie-Dictionary/static/js/runtime-main.0782a8af.js"
+  },
+  {
+    "revision": "f6b331a7c8edd7ed43aa2a860ee2703b",
+    "url": "/Movie-Dictionary/static/media/Spinner.f6b331a7.gif"
+  }
+]);
